c634104b12efe20db3203c181f31fcea948da2a4
Rossi <admin@rassloff.info>
2018-12-03 15:23:35 +0100

    Update default.js

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -2,6 +2,8 @@ export default function(server) {
   /*
     Seed your development database using your factories.
     This data will not be loaded in your tests.
+    
+    ok. here were created 11 dummy users.
   */
 
   server.createList('user', 11);
