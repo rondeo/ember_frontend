4b84b3e55b6641c3bccaaa8955333a5eb16c8948
Rossi <admin@rassloff.info>
2019-01-15 11:10:16 +0100

    some auth fuck

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,5 @@
 import OAuth2PasswordGrantAuthenticator from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrantAuthenticator.extend({
-  serverTokenEndpoint: '/api/login'
+  serverTokenEndpoint: 'http://127.0.0.1:8000/api/login'
 });
